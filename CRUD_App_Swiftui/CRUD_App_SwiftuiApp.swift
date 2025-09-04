683d8b1282e9a6e8bf2ebe0d77657a941376f1b2
Naznin <nazninbasheer@gmail.com>
2025-09-04 21:00:59 +0530

    first commit..

---
+++ CRUD_App_Swiftui/CRUD_App_SwiftuiApp.swift
@@ -0,0 +1,46 @@
+//
+//  CRUD_App_SwiftuiApp.swift
+//  CRUD_App_Swiftui
+//  Created by naznin on 07/08/25.
+
+import SwiftUI
+
+@main
+struct CRUD_App_SwiftuiApp: App {
+    @State private var showSplash = true
+    
+    var body: some Scene {
+        WindowGroup {
+            if showSplash {
+                SplashView()
+                    .onAppear{
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                            withAnimation{
+                                showSplash = false
+                            }
+                        }
+                    }
+            }else{
+                ContentView()
+            }
+           
+        }
+    }
+}
+
+struct SplashView : View{
+    var body: some View{
+        ZStack {
+            Color.brown
+            VStack{
+                Image(systemName: "sparkles")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                
+                Text("Welcome")
+                    .font(.title)
+            }
+            .foregroundColor(.white)
+        }.ignoresSafeArea()
+    }
+}
---
+++ CRUD_App_Swiftui/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  CRUD_App_Swiftui
+//
+//  Created by Naznin on 07/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var list : [String] = []
+    @State private var items : String = ""
+    var body: some View {
+        ZStack{
+            Color.brown.opacity(0.3)
+                .ignoresSafeArea(.all)
+    VStack{
+        Text("ADD ITEMS").font(.title)
+            .fontWeight(.heavy)
+            .padding()
+        
+        TextField("Add items",text:$items)
+            .shadow(color: Color.brown, radius:5)
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.brown,lineWidth: 4))
+            .padding()
+    HStack{
+        Image(systemName: "plus")
+        Button("ADD"){
+            if !items .isEmpty{
+                list.append(items)
+                items = ""
+            }
+        }
+        }.padding()
+            .font(.headline)
+            .foregroundColor(Color.white)
+            .background(.brown.opacity(0.7))
+            .cornerRadius(15)
+        Spacer()
+        
+        Text("ITEMS")
+            .underline()
+            .font(.headline)
+            .fontWeight(.heavy)
+            .padding()
+            
+        List{
+        ForEach(list.indices, id: \.self){
+                index in
+            TextField( "" ,text: $list[index])
+                .padding(8)
+                .foregroundColor(Color.black)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .listRowBackground((index % 2 == 0 ) ? Color.brown.opacity(0.2)
+                    :Color.brown.opacity(0.4))
+              
+        }.onDelete{
+            list.remove(atOffsets: $0)}
+            
+        }.cornerRadius(30)
+            .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.brown,lineWidth: 2))
+            .padding()
+            
+            
+        }
+    }
+ }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
